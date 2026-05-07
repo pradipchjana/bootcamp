0c545f090ffb54234b3bbcb2a25332c285f209f8
Pradip Jana <janaprdip501@gmail.com>
2026-05-07 10:59:44 +0530

    initial setup

---
+++ bootcamp_project/test/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class MainTest {
+    @Test
+    void testIng(){
+        mock();
+    }
+}
