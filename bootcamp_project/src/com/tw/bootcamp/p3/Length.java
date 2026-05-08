6603b70e55734c965ee5cdc72618c1f307df3339
Pradip Jana <janaprdip501@gmail.com>
2026-05-08 11:53:26 +0530

    implements Length Class

---
+++ bootcamp_project/src/com/tw/bootcamp/p3/Length.java
@@ -0,0 +1,53 @@
+package com.tw.bootcamp.p3;
+import java.util.Objects;
+
+public class Length {
+    private static final Double CM_TO_METER = 0.01;
+    private final Double value;
+    private final String unit;
+    private  final Double INCH_TO_METER = 0.0254;
+    private final Double FEET_TO_METER = 0.3048;
+
+    private Length(Double value,String unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    public static Length createLength(Double value,String unit) {
+        return new Length(value,unit);
+    }
+    public static Length createInch(Double value) {
+        return createLength(value,"INCH");
+    }
+    public static Length createFeet(Double value) {
+        return createLength(value,"FEET");
+    }
+
+    public static Length createCentiMeter(double value) {
+        return createLength(value,"CM");
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Length length = (Length) o;
+
+        return toMeter() - length.toMeter() < 0.00001;
+    }
+
+    public Double toMeter() {
+        return switch (this.unit) {
+            case "INCH" -> this.value * this.INCH_TO_METER;
+            case "FEET" -> this.value * this.FEET_TO_METER;
+            case "CM" -> this.value * this.CM_TO_METER;
+
+            default -> null;
+        };
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
---
+++ bootcamp_project/test/com/tw/bootcamp/p3/LengthTest.java
@@ -0,0 +1,20 @@
+package com.tw.bootcamp.p3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LengthTest {
+    @Test
+    void shouldReturnTrueFor1FeetAnd12Inch(){
+        Length feet = Length.createFeet(1.0);
+        Length inch = Length.createInch(12.0);
+        assertEquals(feet,inch);
+    }
+    @Test
+    void shouldReturnTrueFor2InchAnd5Centimeters(){
+        Length centiMeter = Length.createCentiMeter(5.0);
+        Length inch = Length.createInch(2.0);
+        assertEquals(centiMeter,inch);
+    }
+}
